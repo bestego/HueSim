f4fc7ccd01f85f6c3462dabddf7564d5cab919d3
Erik van Gompel <erik@van-gompel.nl>
2020-04-25 22:16:22 +0200

    Initial implementation of ruimte

---
+++ huesim/src/main/java/nl/bestego/huesim/model/Ruimte.java
@@ -0,0 +1,20 @@
+package nl.bestego.huesim.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.ArrayList;
+
+@Entity
+public class Ruimte {
+
+    @Id
+    @Getter @Setter private Long id;
+    @Getter @Setter private String omschrijving;
+    @Getter @Setter private String lampen;
+    @Getter @Setter private boolean aan;
+    @Getter @Setter private int helderheid;
+
+}
--- huesim/src/main/resources/data.sql
+++ huesim/src/main/resources/data.sql
@@ -1,3 +1,4 @@
 insert into lamp(id, omschrijving, aan, helderheid) values(1,'lamp-1',TRUE,75);
 insert into lamp(id, omschrijving, aan, helderheid) values(2,'lamp-2',FALSE,50);
 insert into lamp(id, omschrijving, aan, helderheid) values(3,'lamp-3',TRUE,25);
+insert into ruimte(id, omschrijving, aan, helderheid,lampen) values(0,'allemaal',FALSE,0,'1,2,3')
