0ac18828a8dfbebb2ddee8650841f0514c251d06
Erik van Gompel <erik@van-gompel.nl>
2020-04-30 12:08:35 +0200

    Added logging Lombok style

--- huesim/src/main/java/nl/bestego/huesim/rest/LampEndpoint.java
+++ huesim/src/main/java/nl/bestego/huesim/rest/LampEndpoint.java
@@ -1,5 +1,6 @@
 package nl.bestego.huesim.rest;
 
+import lombok.extern.slf4j.Slf4j;
 import nl.bestego.huesim.control.LampService;
 import nl.bestego.huesim.model.Lamp;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @RestController
 public class LampEndpoint {
 
@@ -20,24 +22,28 @@ public class LampEndpoint {
     @GetMapping("/lamp/{id}")
     public @ResponseBody
     Lamp getStatusLamp(@PathVariable Long id) {
-        System.out.println("getStatusLamp: /lamp/" + id);
+        //System.out.println("getStatusLamp: /lamp/" + id);
+        log.info("getStatusLamp: /lamp/{}", id);
         return service.statusLamp(id);
     }
 
     @GetMapping("/lamp")
     public @ResponseBody
     List<Lamp> getStatusLampen() {
-        System.out.println("getStatusLampen: /lamp");
+        //System.out.println("getStatusLampen: /lamp");
+        log.info("getStatusLampen: /lamp");
         return service.statusLampen();
     }
 
     @PutMapping("/lamp/{id}")
     public @ResponseBody
     ResponseEntity<String> wijzigNaamLamp(@RequestBody @Valid Lamp lamp, @PathVariable Long id) {
-        System.out.println("putWijzigNaamLamp: /lamp/" + id);
-        if ( service.wijzigNaamLamp(lamp, id) ){
+        //System.out.println("putWijzigNaamLamp: /lamp/" + id);
+        if (service.wijzigNaamLamp(lamp, id)) {
+            log.info("putWijzigNaamLamp: /lamp/{}", id);
             return new ResponseEntity<>("OK", HttpStatus.OK);
         } else {
+            log.warn("putWijzigNaamLamp: /lamp/{}", id);
             return new ResponseEntity<>("NOK", HttpStatus.NO_CONTENT);
         }
     }
@@ -46,9 +52,11 @@ public class LampEndpoint {
     public @ResponseBody
     ResponseEntity<String> wijzigStatusLamp(@RequestBody @Valid Lamp lamp, @PathVariable Long id) { //ToDo: test with incomplete body (empty omschrijving)
         System.out.println("putWijzigNaamLamp: /lamp/" + id + "/status");
-        if ( service.wijzigStatusLamp(lamp, id) ){
+        if (service.wijzigStatusLamp(lamp, id)) {
+            log.info("putWijzigNaamLamp: /lamp/{}/status", id);
             return new ResponseEntity<>("OK", HttpStatus.OK);
         } else {
+            log.warn("putWijzigNaamLamp: /lamp/{}/status", id);
             return new ResponseEntity<>("NOK", HttpStatus.NO_CONTENT);
         }
     }
